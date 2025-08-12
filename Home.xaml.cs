a370277d35b38ffd5027dc61005eb434d4c0dea2
Chikezie <chixide12@gmail.com>
2025-08-13 00:17:01 +0100

    added basic list

--- Home.xaml.cs
+++ Home.xaml.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -11,6 +5,12 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
@@ -20,14 +20,16 @@ using Windows.Foundation.Collections;
 namespace TodoApp;
 
 /// <summary>
-/// An empty window that can be used on its own or navigated to within a Frame.
+/// An empty page that can be used on its own or navigated to within a Frame.
 /// </summary>
-public sealed partial class MainWindow : Window
-{    
-    public MainWindow()
+public sealed partial class Home : Page
+{
+    private readonly ObservableCollection<string> _items = [
+        "Clean House", "Do Laundry", "Call Mom", "Call Dad", "Call Joe"
+    ];
+
+    public Home()
     {
         InitializeComponent();
-
-        MainFrame.Navigate(typeof(Home));
     }
 }
